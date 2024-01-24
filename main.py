9406d7c2f64510709a0a619cbd3789eca52c147d
b.dorofeev <b.dorofeev@meotyda.com>
2024-01-24 17:50:09 +0300

    KeyboardInterrupt

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from сonsumer import Consumer
 
 def main():
     conf = load_config('rabbitmq_config.yaml')
+    try:
+        # Запуск продюсера
+        producer = Producer(conf['rmq']['connection'])
+        asyncio.run(producer.send_message(conf['rmq']['test_queue']['params']['name'], "Hello World"))
 
-    # Запуск продюсера
-    producer = Producer(conf['rmq']['connection'])
-    asyncio.run(producer.send_message(conf['rmq']['test_queue']['params']['name'], "Hello World"))
-
-    # Запуск консьюмера
-    consumer = Consumer(conf['rmq']['connection'])
-    asyncio.run(consumer.consume(conf['rmq']['test_queue']['params']['name']))
-
+        # Запуск консьюмера
+        consumer = Consumer(conf['rmq']['connection'])
+        asyncio.run(consumer.consume(conf['rmq']['test_queue']['params']['name']))
+    except KeyboardInterrupt:
+        print("Программа остановлена пользователем")
 if __name__ == "__main__":
     main()
